6903f7d36ebdd2f462a7c48dc448b2345914bba5
Flavio Curella <flavio.curella@gmail.com>
2016-02-19 10:25:06 -0600

    Promisify `appendScript`

--- spectrum-chrome/src/content_scripts/content-script.js
+++ spectrum-chrome/src/content_scripts/content-script.js
@@ -2,31 +2,34 @@ var injectScript = function(func, args) {
   return '(' + func + ')(' + JSON.stringify(args) + ');'
 };
 
-var appendScript = function(fileName, cb) {
-  var s = document.createElement('script');
-  s.class = 'spectrumJs';
-  s.src = chrome.extension.getURL(fileName);
+var appendScript = function(fileName) {
+  var promise = new Promise(function(resolve, reject) {
+    var s = document.createElement('script');
+    s.className = 'spectrumJs';
+    s.src = chrome.extension.getURL(fileName);
 
-  (document.head||document.documentElement).appendChild(s);
-  if (cb !== undefined) {
-    s.onload = cb;    
-  }
-  /*
-  s.onload = function() {
-    s.parentNode.removeChild(s);
-  };
-  */
+    (document.head||document.documentElement).appendChild(s);
+    s.onload = resolve;    
+    /*
+    s.onload = function() {
+      s.parentNode.removeChild(s);
+    };
+    */
+    return s;
+  });
+  return promise;
 }
 
 var i = document.createElement('input');
 i.id = 'spectrumExtensionId';
-i.class = 'spectrumJs';
+i.className = 'spectrumJs';
 i.type = 'hidden';
 i.value = chrome.runtime.id;
 console.log(chrome.runtime);
 document.body.appendChild(i);
 
-appendScript('bundles/common.bundle.js', function() {
+appendScript('bundles/common.bundle.js')
+.then(function() {
   appendScript('bundles/spectrum.bundle.js');  
 });
 
